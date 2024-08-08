079a0eabe2d04b919783bce3876dbe15e0a486ac
zhalok <zhalokrahman007@gmail.com>
2024-08-09 01:57:43 +0600

    changed some file names

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from preprocessing.preprocess import preprocess_raw_dataset
 from preprocessing.prepare import prepare_dataset
 from preprocessing.tokenize import tokenize_and_align_labels
-from training import prepare_trainer, train
+from trainer import prepare_trainer, train
 
 
 preprocess_raw_dataset()
@@ -14,7 +14,6 @@ test_set = prepared_dataset["test_dataset"]
 
 tokenized_train_dataset = train_set.map(tokenize_and_align_labels)
 tokenized_validation_dataset = val_set.map(tokenize_and_align_labels)
-tokenized_test_dataset = test_set.map(tokenize_and_align_labels)
 
 trainer = prepare_trainer(
     train_set=tokenized_train_dataset, val_set=tokenized_validation_dataset
